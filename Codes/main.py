6e9f4fb8273080afd3bd4af2c3bad8d08a9430ca
mrtaz77 <mrtazwar23@gmail.com>
2024-06-10 19:29:49 +0600

    connection init

--- Codes/main.py
+++ Codes/main.py
@@ -25,3 +25,36 @@ def convert_host_order_to_network_order(num):
 def get_mac_address(mac_addr_bytes):
 	mac_addr = map('{:02x}'.format, mac_addr_bytes)
 	return (':'.join(mac_addr)).upper()
+
+def init_connection():
+	'''
+	This line gets the IP address of the local machine.
+	socket.gethostname() retrieves the hostname of the local machine.
+	socket.gethostbyname() converts the hostname to its corresponding IP address.
+	HOST will hold the local IP address.
+	'''
+	HOST = socket.gethostbyname(socket.gethostname())
+	'''
+	This creates a raw socket.
+	socket.AF_INET specifies the address family for IPv4.
+	socket.SOCK_RAW specifies that this is a raw socket, allowing for low-level network packet access.
+	'''
+	conn = socket.socket(socket.AF_INET, socket.SOCK_RAW)
+	'''
+	This binds the raw socket to the local IP address (HOST) and an arbitrary port number (0).
+	Binding to port 0 tells the operating system to choose an arbitrary available port.
+	'''
+	conn.bind((HOST, 0))
+	'''
+	This uses the ioctl (input/output control) method to enable promiscuous mode on the socket.
+	socket.SIO_RCVALL is a socket I/O control code specific to Windows that enables a socket to receive all packets.
+	socket.RCVALL_ON is the flag to turn on promiscuous mode.
+	In promiscuous mode, the network interface card (NIC) captures all packets on the network, regardless of their destination.
+	NOTE : Creating raw sockets and enabling promiscuous mode typically requires elevated permissions (administrator or root access).
+	'''
+	conn.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)	
+	'''
+	Summary : Get local IP-addr > create raw socket for IPv4 > Bind to local IP on any port > enable promiscuous mode
+	'''
+	return conn
+
