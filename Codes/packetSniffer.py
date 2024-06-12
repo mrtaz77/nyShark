27b0b193a7977e37108c899b32a3b8276f3b2ba2
mrtaz77 <mrtazwar23@gmail.com>
2024-06-12 23:29:50 +0600

    final packetsniffer

---
+++ Codes/packetSniffer.py
@@ -0,0 +1,36 @@
+from layers.EthernetFrame import EthernetFrame
+from artwork import *
+import socket
+
+BUFFER_SIZE = 65536
+
+def init_connection():
+	'''
+	- Creating the Socket:
+		conn = socket.socket(...) initializes a new socket and assigns it to the variable conn.
+	- Address Family (AF_PACKET):
+		socket.AF_PACKET specifies that this socket will operate at the link layer, meaning it will handle raw Ethernet frames directly.
+	- Socket Type (SOCK_RAW):
+		socket.SOCK_RAW specifies that this socket will handle raw packets, providing access to the entire packet, including headers and payloads.
+	- Protocol (ntohs(3)):
+		socket.ntohs(3) specifies the protocol. 
+		By using socket.ntohs, the protocol number is converted from network byte order to host byte order.
+		The value 3 corresponds to ETH_P_ALL, meaning the socket will capture all Ethernet protocols.
+	'''
+	return socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+def sniff():
+	conn = init_connection()
+	layer = EthernetFrame()
+	try:
+		while True:
+			raw_data, address = conn.recvfrom(BUFFER_SIZE)
+			layer.unpack(raw_data)
+			layer.show()
+	except KeyboardInterrupt:
+		conn.close()
+
+if __name__ == '__main__':
+	init()
+	nyShark_artwork()
+	sniff()
