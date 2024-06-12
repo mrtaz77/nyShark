4f309c67c735f3ed84742d6e2d1774b0e37451bb
mrtaz77 <mrtazwar23@gmail.com>
2024-06-12 15:33:21 +0600

    added artwork

--- Codes/formatting.py
+++ Codes/formatting.py
@@ -2,9 +2,24 @@
 TAB = '\t'
 SPACE_HYPHEN = ' - '
 SPACE = ' '
+DOT = '.'
 
 def indent(n):
     return n * TAB + SPACE_HYPHEN
 
 def data_indent(n):
 	return n * TAB + SPACE
+
+def dots(n):
+    return n * DOT
+
+
+def nyShark_artwork():
+    print("""
+                _____ __               __  
+   ____  __  __/ ___// /_  ____ ______/ /__
+  / __ \/ / / /\__ \/ __ \/ __ `/ ___/ //_/
+ / / / / /_/ /___/ / / / / /_/ / /  / ,<   
+/_/ /_/\__, //____/_/ /_/\__,_/_/  /_/|_|  
+      /____/                               
+""")
