b9d487fb61b76180373825a1a0f82b21ff4483a7
mrtaz77 <mrtazwar23@gmail.com>
2024-06-12 16:32:28 +0600

    mov artwork and init colorful display

---
+++ Codes/artwork.py
@@ -0,0 +1,17 @@
+from colorama import init, Fore
+
+def nyShark_artwork():
+    print(Fore.LIGHTRED_EX + """
+                _____ __               __  
+   ____  __  __/ ___// /_  ____ ______/ /__
+  / __ \/ / / /\__ \/ __ \/ __ `/ ___/ //_/
+ / / / / /_/ /___/ / / / / /_/ / /  / ,<   
+/_/ /_/\__, //____/_/ /_/\__,_/_/  /_/|_|  
+      /____/                               
+""")
+
+init()
+RED = Fore.LIGHTRED_EX
+GREEN = Fore.GREEN
+BLUE = Fore.BLUE
+YELLOW = Fore.YELLOW
